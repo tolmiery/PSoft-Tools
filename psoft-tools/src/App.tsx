636cbfd0c3db1b9cae5fa816e4521440ad971a3f
Johnny Sun <jsun8934@gmail.com>
2024-02-02 17:04:45 -0500

    Set up File Routing
    Someone downloaded to root... Front end file routing set up!

--- psoft-tools/src/App.tsx
+++ psoft-tools/src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider, LoaderFunction, ActionFunction } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src='./assets/react.svg' className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src='/vite.svg' className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface RouteCommon {
+  loader ?: LoaderFunction;
+  action ?: ActionFunction;
+  ErrorBoundary ?: React.ComponentType<any>;
 }
 
-export default App
+interface Routes extends RouteCommon {
+  path:string;
+  Element:React.ComponentType<any>;
+}
+
+interface Pages {
+  [key: string]: {
+    default: React.ComponentType<any>;
+  } & RouteCommon;
+}
+
+const pages: Pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
+
+const routes: Routes[] = [];
+for (const path of Object.keys(pages)) {
+  const fileName = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1];
+  if (!fileName) {
+    continue;
+  }
+
+  const normalizedPathName = fileName.includes("$")
+    ? fileName.replace("$", ":")
+    : fileName.replace(/\/index/, "");
+
+  routes.push({
+    path: fileName === "index" ? "/" : `/${normalizedPathName.toLowerCase()}`,
+    Element: pages[path].default,
+    loader: pages[path]?.loader,
+    action: pages[path]?.action,
+    ErrorBoundary: pages[path]?.ErrorBoundary,
+  });
+}
+
+const router = createBrowserRouter(
+  routes.map(({ Element, ErrorBoundary, ...rest }) => ({
+    ...rest,
+    element: <Element />,
+    ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
+  }))
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
