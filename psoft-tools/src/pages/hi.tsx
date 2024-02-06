72950e2a3deebf822f0f58a3cba8bb9f47057014
Johnny Sun <jsun8934@gmail.com>
2024-02-06 16:34:20 -0500

    Navbar basics created
    Navbar css made tsx started.
    
    Co-Authored-By: eatdegrasstyson <75918528+eatdegrasstyson@users.noreply.github.com>

--- psoft-tools/src/pages/hi.tsx
+++ psoft-tools/src/pages/hi.tsx
@@ -1,3 +1,3 @@
 export default function Hi(){
-    return <>Hi</>;
+    return <>I love benching</>;
 }
--- psoft-tools/src/pages/index.tsx
+++ psoft-tools/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Editor } from "@monaco-editor/react";
+import { Editor } from "@monaco-editor/react"; 
 
 //Create Routing File
 export default function Index() {
