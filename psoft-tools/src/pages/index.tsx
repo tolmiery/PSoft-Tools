d2d09d35ca67babeee2fb780a09cef3d8de6720c
Johnny Sun <jsun8934@gmail.com>
2024-02-23 17:20:57 -0500

    Test on Fetch

--- psoft-tools/src/pages/index.tsx
+++ psoft-tools/src/pages/index.tsx
@@ -1,7 +1,19 @@
 import { Editor } from "@monaco-editor/react"; 
 import Navbar from "../components/Navbar";
+import { useState } from "react";
 
 //Create Routing File
+const [id, setId] = useState('')
+const [data, setData] = useState(null)
+
+const handleClick = async () => {
+  try{
+    //const data = push();
+    setData(data)
+  } catch (err) {
+    console.log("mistake")
+  }
+}
 export default function Index() {
   return (
     <div>
@@ -11,14 +23,21 @@ export default function Index() {
       <div className="screen" style={{ paddingTop: "50px", width: "100%", overflow: "hidden" }}>
         <div style = {{ width: "50%", justifyContent: "left" }}>
           <Editor
-            height="100vh"
+            height="92vh"
             width="50vw"
             defaultLanguage="javascript"
             defaultValue="// input code"
           />
         </div>
         <div className="flex" style={{ justifyContent: "center" }}>
-          Output.Output.Output.Output
+          <div style={{top:50}}>data</div>
+          <button 
+            className="button"
+            style={{ position: "absolute", right: 10, bottom: 10}}
+            onClick={handleClick}
+            >
+              Compile
+            </button>     
         </div>
       </div>
     </div>
