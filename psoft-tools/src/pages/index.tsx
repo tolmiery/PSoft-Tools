da19dfb0f0951725d4f85a9a17ba7256faa5a4af
Johnny Sun <jsun8934@gmail.com>
2024-03-12 17:12:10 -0400

    Merge
    Joseph did most of this!
    
    Co-Authored-By: Enigma-iwnl <108947845+Enigma-iwnl@users.noreply.github.com>

--- psoft-tools/src/pages/index.tsx
+++ psoft-tools/src/pages/index.tsx
@@ -1,27 +1,16 @@
 import { Editor } from "@monaco-editor/react";
 import Navbar from "../components/Navbar";
-<<<<<<< HEAD
 import { useState } from "react";
 import { get } from "../lib/api";
-=======
-import { useEffect, useState } from "react";
->>>>>>> main
+
 
 //Create Routing File
 
 export default function Index() {
-<<<<<<< HEAD
   const [data, setData] = useState("hello");
-  const handleClick = async () => {
-    try {
-      //const data = push();
-      setData(data);
-    } catch (err) {
-      console.log("mistake");
-    }
-  };
-  const [id, setId] = useState("");
-  const handleClick1 = () => {
+  const [code, setCode] = useState("");
+  
+  const handleClick = () => {
     get("http://localhost:3000")
       .then(response => {
         console.log(response);
@@ -30,18 +19,14 @@ export default function Index() {
         console.error("error: ", error);
       })
   };
-=======
-  const [code, setCode] = useState("");
+  
   const handleEditorChange = (value: string | undefined) => {
     if (value) {
       setCode(value);
     }
   };
 
-  useEffect(() => {
-    console.log(code);
-  }, [code]);
->>>>>>> main
+  
   return (
     <div>
       <div>
@@ -62,7 +47,7 @@ export default function Index() {
         </div>
         <div style={{top: 50, right: 50}}>{data}</div>
         <button 
-          onClick={handleClick1}
+          onClick={handleClick}
           style={{position: "absolute", right: 10, bottom: 10}}
           >Click me</button>
       </div>
