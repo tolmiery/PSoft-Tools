05bc15058ff8bceb6e07ad50d012f6e92d1b3452
eatdegrasstyson <jainarnav2811@gmail.com>
2024-03-29 18:36:59 -0400

    Removed 3 boxes and minor UI fix

--- psoft-tools/src/pages/index.tsx
+++ psoft-tools/src/pages/index.tsx
@@ -4,26 +4,13 @@ import { useEffect, useState } from "react";
 
 //Create Routing File
 export default function Index() {
-  const [precondition, setPrecondition] = useState("");
   const [code, setCode] = useState("");
-  const [postcondition, setPostcondition] = useState("");
   
-  const handlePreEditorChange = (value: string | undefined) => {
-    if(value) {
-      setPrecondition(value);
-    }
-  };
   const handleEditorChange = (value: string | undefined) => {
     if (value) {
       setCode(value);
     }
   };
-  const handlePostEditorChange = (value: string | undefined) => {
-    if(value) {
-      setPostcondition(value);
-    }
-  };
-
 
   useEffect(() => {
     console.log(code);
@@ -38,27 +25,15 @@ export default function Index() {
         style={{ paddingTop: "50px", width: "100%", overflow: "hidden" }}
       >
         <div style={{ width: "50%", justifyContent: "left" }}>
+         
           <Editor
-            height="10vh"
-            width="50vw"
-            defaultLanguage="javascript"
-            defaultValue="// input precondition"
-            onChange={handlePreEditorChange}
-          />
-          <Editor
-            height="72vh"
+            height="100vh"
             width="50vw"
             defaultLanguage="javascript"
             defaultValue="// input code"
             onChange={handleEditorChange}
           />
-          <Editor
-            height="10vh"
-            width="50vw"
-            defaultLanguage="javascript"
-            defaultValue="// input postcondition"
-            onChange={handlePostEditorChange}
-          />
+          
         </div>
         <div className="flex" style={{ justifyContent: "center" }}>
           <div style={{top:50}}>Output. Output</div>
