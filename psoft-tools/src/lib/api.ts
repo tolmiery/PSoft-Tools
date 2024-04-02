289a8075fd744922018a430ace23214140e28bf5
Johnny Sun <jsun8934@gmail.com>
2024-04-02 17:03:43 -0400

    Fixed UI/UX
    Parsing in backend, don't need three editors for each section. Also created example for users to follow. Set output properly. Fixed all bugs.

--- psoft-tools/src/lib/api.ts
+++ psoft-tools/src/lib/api.ts
@@ -1,4 +1,4 @@
-export async function post(url: string, body: object) {
+export async function post(url: string, body: string) {
   const response = await fetch(url, {
     method: "POST",
     headers: {
--- psoft-tools/src/pages/index.tsx
+++ psoft-tools/src/pages/index.tsx
@@ -1,20 +1,41 @@
 import { Editor } from "@monaco-editor/react";
 import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
+import { post } from "../lib/api.ts"
+
+
 
 //Create Routing File
+const DEFAULT_CODE = "//Input Code(Delete me before running)"
+const DEFAULT_DATA = "Dafny Confirmed Validity:"
+
 export default function Index() {
-  const [code, setCode] = useState("");
+  const [data, setData] = useState(DEFAULT_DATA);
+  const [code, setCode] = useState(DEFAULT_CODE);
   
+  const handleClick = () => {
+    post("http://localhost:3000/Hoars_Tripple", code)
+      .then(response => {
+        //console.log("HI");
+        console.log(response);
+
+        setData(DEFAULT_DATA + response.toString());
+      })  
+      .catch(error => {
+        console.error("error: ", error);
+      })
+  };
+
+  const handleClickClear = () => {
+    setCode(DEFAULT_CODE); 
+    setData(DEFAULT_DATA);
+  };
   const handleEditorChange = (value: string | undefined) => {
     if (value) {
       setCode(value);
     }
   };
-
-  useEffect(() => {
-    console.log(code);
-  }, [code]);
+  
   return (
     <div>
       <div>
@@ -25,25 +46,34 @@ export default function Index() {
         style={{ paddingTop: "50px", width: "100%", overflow: "hidden" }}
       >
         <div style={{ width: "50%", justifyContent: "left" }}>
-         
+          <div style={{ marginBottom: "10px" }}>
+            <p>Please input code like the following example below:</p>
+            <p>
+              {/* Example code */}
+              <code style = {{ whiteSpace: "pre-wrap"}}>
+                {"{precondition}\ncode\n{postcondition}"}
+              </code>
+            </p>
+          </div>
           <Editor
-            height="100vh"
+            height="75vh"
             width="50vw"
             defaultLanguage="javascript"
-            defaultValue="// input code"
+            value={code}
             onChange={handleEditorChange}
           />
-          
         </div>
-        <div className="flex" style={{ justifyContent: "center" }}>
-          <div style={{top:50}}>Output. Output</div>
-          <button 
-            className="button"
-            style={{ position: "absolute", right: 10, bottom: 10}}
-            >
-              Compile
-            </button>     
+        <div style={{position: "relative", paddingLeft: 15, whiteSpace: "pre-line", textAlign: "start", tabSize: 5, alignSelf: "center", marginLeft: "25%"}}>
+          {data}
         </div>
+        <button 
+          onClick={handleClickClear}
+          style={{position: "absolute", right: 150, bottom: 10}}
+          >Clear</button>
+        <button 
+          onClick={handleClick}
+          style={{position: "absolute", right: 10, bottom: 10}}
+          >Run Dafny</button>
       </div>
     </div>
   );
