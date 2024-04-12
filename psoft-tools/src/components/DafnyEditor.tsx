4ccbae3e6e1603c8aa8311b4579b95a23dbadb0e
Enigma-iwnl <108947845+Enigma-iwnl@users.noreply.github.com>
2024-04-12 17:21:04 -0400

    cleanup

--- psoft-tools/src/components/DafnyEditor.tsx
+++ psoft-tools/src/components/DafnyEditor.tsx
@@ -2,6 +2,7 @@ import { EditorProps, useMonaco } from "@monaco-editor/react";
 import { useEffect } from "react";
 import { Editor } from "@monaco-editor/react";
 import useDafny from "../hooks/useDafny";
+
 export default function DafnyEditor({
   EditorProps,
 }: {
@@ -9,26 +10,36 @@ export default function DafnyEditor({
 }) {
   const monaco = useMonaco();
   const dafny = useDafny();
+  //cdslkmkl
   const language = dafny.langDef;
   useEffect(() => {
+    let cleanup: null | (() => void) = null;
     if (monaco) {
       monaco.languages.register({ id: "dafny" });
       monaco.languages.setMonarchTokensProvider("dafny", language);
 
-      monaco.languages.registerCompletionItemProvider("dafny", {
-        provideCompletionItems: (model, position) => {
-          const word = model.getWordUntilPosition(position);
-          const range = {
-            startLineNumber: position.lineNumber,
-            endLineNumber: position.lineNumber,
-            startColumn: word.startColumn,
-            endColumn: word.endColumn,
-          };
+      const { dispose } = monaco.languages.registerCompletionItemProvider(
+        "dafny",
+        {
+          provideCompletionItems: (model, position) => {
+            const word = model.getWordUntilPosition(position);
+            const range = {
+              startLineNumber: position.lineNumber,
+              endLineNumber: position.lineNumber,
+              startColumn: word.startColumn,
+              endColumn: word.endColumn,
+            };
 
-          return { suggestions: dafny.suggestionsWithRange(range) };
-        },
-      });
+            return { suggestions: dafny.suggestionsWithRange(range) };
+          },
+        }
+      );
+      cleanup = dispose;
     }
+    return () => {
+      if (!monaco || !cleanup) return;
+      cleanup();
+    };
   }, [monaco]);
 
   return <Editor {...EditorProps} />;
