17595b989efcbc5a7675ef5d757fd62434843a79
RyanCarroll7 <ryan.carroll421@gmail.com>
2024-06-11 15:23:29 -0400

    DafnyParser now assigns values to postVariables in dafny code prior to given java code

--- psoft-tools/src/lib/DafnyParser.ts
+++ psoft-tools/src/lib/DafnyParser.ts
@@ -1,11 +1,13 @@
 // requires that the java code be in the form {pre} code {post}
-export default function dafnyParser(triple: String){
+export default function dafnyParser(triple: String) {
     console.log(`Code: ` + triple);
     const hoareTriple = triple.split(/\n/g).filter(Boolean); // [pre, code, post]
+    console.log(hoareTriple);
     // hoareTriple[0] = hoareTriple[0].split(/{([^}]*)}/g).filter(Boolean)[0];
     // hoareTriple[2] = hoareTriple[2].split(/{([^}]*)}/g).filter(Boolean)[0];
     hoareTriple[0] = hoareTriple[0].split(/{([^}]*)}/g).filter(Boolean)[0];
-    hoareTriple[hoareTriple.length-1] = hoareTriple[hoareTriple.length-1].split(/{([^}]*)}/g).filter(Boolean)[0];
+    console.log(hoareTriple);
+    hoareTriple[hoareTriple.length - 1] = hoareTriple[hoareTriple.length - 1].split(/{([^}]*)}/g).filter(Boolean)[0];
     console.log(hoareTriple);
     let resultCode: string = "";
     let methodHeader: string = "method test(";
@@ -18,16 +20,16 @@ export default function dafnyParser(triple: String){
 
     // find how many variables exist in the precondition
     let preVariables: Set<string> = new Set<string>();
-    for(var character of hoareTriple[0]){
-        if((character >= 'a'&& character <= 'z') || (character >= 'A' && character <= 'Z') && !preVariables.has(character)){
+    for (var character of hoareTriple[0]) {
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z') && !preVariables.has(character)) {
             preVariables.add(character);
         }
     }
     let counter: number = 0;
-    for(var variable of preVariables){
+    for (var variable of preVariables) {
         methodHeader += variable + ": int";
         counter += 1;
-        if(counter !== preVariables.size){
+        if (counter !== preVariables.size) {
             methodHeader += ",";
         }
     }
@@ -35,20 +37,20 @@ export default function dafnyParser(triple: String){
 
     // get variables to return from postcondition
     let postVariables: Set<string> = new Set<string>();
-    for(let i = 1; i < hoareTriple.length - 1; ++i){
-        for(var character of hoareTriple[i]){
-            if((character >= 'a'&& character <= 'z') || (character >= 'A' && character <= 'Z') && !postVariables.has(character)){
+    for (let i = 1; i < hoareTriple.length - 1; ++i) {
+        for (var character of hoareTriple[i]) {
+            if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z') && !postVariables.has(character)) {
                 postVariables.add(character);
             }
         }
     }
-    
+
     methodHeader += "returns (";
     counter = 0;
-    for(var variable of postVariables){
+    for (var variable of postVariables) {
         methodHeader += variable + "_post: int";
         counter += 1;
-        if(counter !== postVariables.size){
+        if (counter !== postVariables.size) {
             methodHeader += ",";
         }
     }
@@ -57,11 +59,11 @@ export default function dafnyParser(triple: String){
     // put in pre and postcondition
     methodHeader += "requires " + hoareTriple[0] + "\n";
     methodHeader += "ensures ";
-    for(var postCharacter of hoareTriple[hoareTriple.length - 1]){
+    for (var postCharacter of hoareTriple[hoareTriple.length - 1]) {
         methodHeader += postCharacter;
-        // if we have a variable in our postcondition we append _method to it
-        if(postVariables.has(postCharacter)){
-            methodHeader +="_post";
+        // if we have a variable in our postcondition we append _post to it
+        if (postVariables.has(postCharacter)) {
+            methodHeader += "_post";
         }
     }
     methodHeader += "{\n";
@@ -69,16 +71,27 @@ export default function dafnyParser(triple: String){
     // add in statements
     // const statements = triple[1].split("\n");
     let methodBody = "";
-    for(let i = 1; i < hoareTriple.length-1; ++i){
-        for(let statementCharacter of hoareTriple[i]){
-            if(postVariables.has(statementCharacter)){
+
+    // Set initial value of each postVariable in the method
+    for (let postVariable of postVariables) {
+        // all postVariables have "_post" appended, for simplicity
+        if (preVariables.has(postVariable)) {
+            // if a postVariable is part of the precondition give it the same value
+            // as the corresponding preVariable
+            methodBody += postVariable + "_post := " + postVariable + ";\n";
+        }
+    }
+
+    for (let i = 1; i < hoareTriple.length - 1; ++i) {
+        for (let statementCharacter of hoareTriple[i]) {
+            if (postVariables.has(statementCharacter)) {
                 methodBody += statementCharacter + "_post";
             }
-            else if(statementCharacter === "="){
-                methodBody +=  ':' +statementCharacter;
+            else if (statementCharacter === "=") {
+                methodBody += ':' + statementCharacter;
             }
-            else{
-                methodBody += statementCharacter ;
+            else {
+                methodBody += statementCharacter;
             }
         }
         methodBody += '\n';
