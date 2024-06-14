c0e7265d2c436136c6aa261792f6505d899fab7c
Luke-Kissling <luke.kissling@gmail.com>
2024-06-14 15:18:14 -0400

    fixed the forward reasoning output to align left and recognize newlines

--- psoft-tools/src/pages/ForwardReasoning.tsx
+++ psoft-tools/src/pages/ForwardReasoning.tsx
@@ -3,53 +3,66 @@ import { Editor } from "@monaco-editor/react";
 import { useState } from "react";
 import { post } from "../lib/api";
 import { ThreeDots } from "react-loader-spinner";
-import dafnyParser from "../lib/DafnyParser";
 
-export default function ForwardReasoning(){
+export default function ForwardReasoning() {
     const [data, setData] = useState("");
     const [code, setCode] = useState("");
     const [loading, setLoading] = useState(false);
+
     const handleClickClear = () => {
         setData("");
-        //setCode("// input code");
-     };
-  
-     const handleEditorChange = (value: string | undefined) => {
+        setCode("// input code");
+    };
+
+    const handleReasoning = () => {
+        setLoading(true);
+        console.log(code);
+        post("http://localhost:3000/forward-reasoning", code)
+            .then((response) => {
+                setLoading(false);
+                console.log(response);
+                setData(response);
+            })
+            .catch((error) => {
+                console.error("error: ", error);
+            });
+    }
+    const handleEditorChange = (value: string | undefined) => {
         if (value) {
-           //console.log(value);
-           setCode(value);
-           //console.log(code);
+            //console.log(value);
+            setCode(value);
+            //console.log(code);
         }
-     };
+    };
     return (
         <div>
-          <div>
-            <Navbar />
-          </div>
-          <div
-            className="screen"
-            style={{ paddingTop: "50px", width: "100%", overflow: "hidden" }}
-          >
-            <div style={{ width: "50%", justifyContent: "left" }}>
-    
-              <Editor height="92vh" width="50vw" onChange={handleEditorChange} defaultLanguage="java"
-                defaultValue='// Input code for forward reasoning'/>
+            <div>
+                <Navbar />
             </div>
-            <div className="flex flex-col justify-center relative pl-8">
-              <div className=" flex-grow">
-                {loading ? (
-                  <ThreeDots color="gray" height={100} width={100} />
-                ) : (
-                  data
-                )}
-              </div>
-              <div className="flex flex-row justify-evenly max-h-11 mb-4">
-                <button onClick={handleClickClear}>Clear</button>
-                <button onClick={handleClickClear}>Forward Reasoning</button>
-              </div>
+            <div
+                className="screen"
+                style={{ paddingTop: "50px", width: "100%", overflow: "hidden" }}
+            >
+                <div style={{ width: "50%", justifyContent: "left" }}>
+
+                    <Editor height="92vh" width="50vw" onChange={handleEditorChange} defaultLanguage="dafny"
+                        defaultValue='// Input should be in the format {precondition} {code}' />
+                </div>
+                <div className="flex flex-col  relative pl-8">
+                    <div className=" flex-grow" style={{ whiteSpace: "pre", textAlign:"left"}}>
+                        {loading ? (
+                            <ThreeDots color="gray" height={100} width={100} />
+                        ) : (
+                            data
+                        )}
+                    </div>
+                    <div className="flex flex-row justify-evenly max-h-11 mb-4">
+                        <button onClick={handleClickClear}>Clear</button>
+                        <button onClick={handleReasoning}>Forward Reasoning</button>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      );
+    );
 }
 
