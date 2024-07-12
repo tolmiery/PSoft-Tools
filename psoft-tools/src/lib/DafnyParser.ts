77dc0d17d6d2e69e8f42c804f0f3c16cbd7c725c
RyanCarroll7 <ryan.carroll421@gmail.com>
2024-07-12 15:08:49 -0400

    Added support for keywords "true" and "false", as well as symbol "!="

--- psoft-tools/src/lib/DafnyParser.ts
+++ psoft-tools/src/lib/DafnyParser.ts
@@ -3,12 +3,12 @@ export default function dafnyParser(triple: String) {
     // Regex to find valid variable names
     const validDafnyVarRegex = /[a-zA-Z?'][a-zA-Z_?'0-9]*/g;
     // Realistically, we don't need to account for most keywords, only those that should be present in a hoare triple
-    const keywords = ["if", "else"];
+    const keywords = ["if", "else", "true", "false"];
     console.log(`Code: ` + triple);
     const hoareTriple = triple.split(/\n/g).filter(Boolean); // [pre, code, post]
     // Remove the brackets from the pre/postcondition
-    hoareTriple[0] = hoareTriple[0].split(/{([^}]*)}/g).filter(Boolean)[0].trim();
-    hoareTriple[hoareTriple.length - 1] = hoareTriple[hoareTriple.length - 1].split(/{([^}]*)}/g).filter(Boolean)[0].trim();
+    let precondition = hoareTriple[0].split(/{([^}]*)}/g).filter(Boolean)[0].trim();
+    let postcondition = hoareTriple[hoareTriple.length - 1].split(/{([^}]*)}/g).filter(Boolean)[0].trim();
     let resultCode: string = "";
     let methodHeader: string = "method test(";
     // each variable in the precondition should be an argument to the Main method.
@@ -19,7 +19,11 @@ export default function dafnyParser(triple: String) {
     // the dafny code
 
     // find how many variables exist in the precondition
-    let preVariables: Set<string> = new Set(hoareTriple[0].match(validDafnyVarRegex));
+    let preVariables: Set<string> = new Set(precondition.match(validDafnyVarRegex));
+    // Remove any keywords (likely true and false)
+    for (var keyword of keywords) {
+        preVariables.delete(keyword);
+    }
     let counter: number = 0;
     for (var variable of preVariables) {
         methodHeader += variable + "_pre: int";
@@ -30,21 +34,20 @@ export default function dafnyParser(triple: String) {
     }
     methodHeader += ") ";
 
-    // get variables to return from postcondition
-    let postVariables: Set<string> = new Set<string>();
+    // To ensure all variables are defined, we tell Dafny to return any variables found in the method body and postcondition
+    let postVariables: Set<string> = new Set<string>(postcondition.match(validDafnyVarRegex));
     for (let i = 1; i < hoareTriple.length - 1; ++i) {
         postVariables = new Set<string>([...postVariables, ...new Set(hoareTriple[i].match(validDafnyVarRegex))]);
     }
-    console.log(postVariables);
-
+    // Remove all keywords from postVariables
+    for (var keyword of keywords) {
+        postVariables.delete(keyword);
+    }
+    // Build our returns clause
     methodHeader += "returns (";
     counter = 0;
     for (var postVariable of postVariables) {
-        counter += 1;
-        if (keywords.includes(postVariable)) {
-            // postVariables.delete(postVariable);
-            continue;
-        }
+        counter++;
         methodHeader += postVariable + ": int";
         if (counter !== postVariables.size) {
             methodHeader += ",";
@@ -52,13 +55,13 @@ export default function dafnyParser(triple: String) {
     }
     methodHeader += ")\n";
 
-    // find and replace each postVariable
+    // find and replace each preVariable in the precondition with "<preVariable>_pre"
     for (var preVariable of preVariables) {
-        hoareTriple[0] = hoareTriple[0].replace(new RegExp(preVariable, 'g'), preVariable + "_pre");
+        precondition = precondition.replace(new RegExp(preVariable, 'g'), preVariable + "_pre");
     }
     // put in pre and postcondition
-    methodHeader += "requires " + hoareTriple[0] + "\n";
-    methodHeader += "ensures " + hoareTriple[hoareTriple.length - 1] + "{\n";
+    methodHeader += "requires " + precondition + "\n";
+    methodHeader += "ensures " + postcondition   + "\n{\n";
 
     // add in statements
     let methodBody = "";
@@ -75,10 +78,10 @@ export default function dafnyParser(triple: String) {
 
     for (let i = 1; i < hoareTriple.length - 1; ++i) {
         for (let j = 0; j < hoareTriple[i].length; j++) {
-            if (hoareTriple[i][j] === "=") {
-                // Check for '==' or '='
+            if (hoareTriple[i][j] === "=" || hoareTriple[i][j] === "!" ) {
+                // Check for '==' or '=' or '!='
                 if (hoareTriple[i][j + 1] === "=") {
-                    // if this is an 'a == b' statement, print this '=' and increment j
+                    // if this is an 'a == b' or 'a != b' statement, print current char and increment j
                     methodBody += hoareTriple[i][j++];
                 }
                 else {
