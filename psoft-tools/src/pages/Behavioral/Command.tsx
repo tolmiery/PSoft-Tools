b60eab34bd9789498c7b968d10256e9c209d2114
Pratheet.Joshi <101433332+DaCatDude@users.noreply.github.com>
2024-12-10 13:28:58 -0500

    adding future development files

---
+++ psoft-tools/src/pages/Behavioral/Command.tsx
@@ -0,0 +1,6 @@
+export default function Command() {
+
+    <div>
+        Command Design Pattern
+    </div>
+}
---
+++ psoft-tools/src/pages/Behavioral/State.tsx
@@ -0,0 +1,6 @@
+export default function State() {
+
+    <div>
+        State Design Pattern
+    </div>
+}
