bf0ab4dcbd315f43ca13d8b1fedd55bd10cae16d
tolmiery <mhiffa@gmail.com>
2025-03-25 17:32:17 -0400

    building an Expression class to aid our grammar

--- psoft-tools/src/lib/HoareTripleTemplate.py
+++ psoft-tools/src/lib/HoareTripleTemplate.py
@@ -1,5 +1,13 @@
 import random
 import string
+import operator as op
+ops = {
+    '+': op.add,
+    '-': op.sub,
+    '*': op.mul,
+    '/': op.truediv,
+    '%': op.mod
+}
 
 """ 
 Grammar rules:
@@ -49,3 +57,72 @@ def getBool():
 #returns a list with all varibles
 def getVariables():
     return VARIABLES
+
+
+"""Class to represent an expression to be assigned to a variable in Hoare Triple Generation.
+    The factors are numbers and variables, while the operators are the arithmetic operators.
+    We map a pair of factors to their operator.
+    """
+class Expression:
+    factors = []  # list of factors (numbers and variables)
+    operators = []  # list of operators (arithmetic operators) should be len(factors) - 1 unless both empty
+    def __init__(self, factors, operators):
+        self.factors = factors
+        self.operators = operators
+
+    def __str__(self):
+        expString = ""
+        for i in range(len(self.factors)):
+            expString += str(self.factors[i])
+            if i < len(self.operators):
+                expString += " " + str(self.operators[i]) + " "
+        return expString
+    
+    def hasMultiplication(self):
+        """Returns true if the expression has a multiplication operator."""
+        return "*" in self.operators
+    
+    def hasDivision(self):
+        """Returns true if the expression has a division operator."""
+        return "/" in self.operators
+    
+    def hasAddition(self):
+        """Returns true if the expression has an addition operator."""
+        return "+" in self.operators
+    
+    def hasSubtraction(self):
+        """Returns true if the expression has a subtraction operator."""
+        return "-" in self.operators
+    
+    def condense(self):
+        """Condenses the expression by removing any redundant operators.
+        Do we care about PEMDAS? maybe."""
+        condensed_factors = []
+        condensed_operators = []
+        for i in range(len(self.factors)-1):
+            if self.operators[i] == "*" or self.operators[i] == "/":
+                try:
+                    condensed_factor = str(ops[self.operators[i]](int(self.factors[i]), int(self.factors[i+1])))
+                    condensed_factors.append(condensed_factor)
+                except:
+                    if self.factors[i] == self.factors[i+1]  and self.operators[i] == "/":
+                        condensed_factors.append("1")
+                    else:
+                        condensed_factors.append(str(self.factors[i]))
+                        condensed_factors.append(str(self.factors[i+1]))
+                        condensed_operators.append(self.operators[i])
+        for i in range(len(condensed_factors)-1):
+            if condensed_operators[i] == "+" or condensed_operators[i] == "-":
+                try:
+                    condensed_factor = str(ops[condensed_operators[i]](int(condensed_factors[i]), int(condensed_factors[i+1])))
+                    condensed_factors.append(condensed_factor)
+                except:
+                    if condensed_factors[i] == condensed_factors[i+1]  and condensed_operators[i] == "-":
+                        condensed_factors.append("0")
+                    elif condensed_factors[i] == condensed_factors[i+1]  and condensed_operators[i] == "+":
+                        condensed_factors.append("2" + str(condensed_factors[i]))
+                    else:
+                        condensed_factors.append(str(condensed_factors[i]))
+                        condensed_factors.append(str(condensed_factors[i+1]))
+                        condensed_operators.append(condensed_operators[i])
+                
